7d3d0e501e846305e263730f3cf53e51ca91ba5e
JimenaNayeliTutillo <93277934+JimenaNayeliTutillo@users.noreply.github.com>
2021-10-29 22:53:15 -0500

    Add files via upload

---
+++ las/clase1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>//salida y entrada de datos
+using namespace std;
+
+int main(){
+    char nombre[30];
+    cout << "Hola mundo\n";
+    gets(nombre);
+    cin>> nombre;
+return 0;
+}
